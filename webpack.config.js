632927e2471db72ac7b77fe8fc81fee0183a9773
sectore <info@websector.de>
2016-09-05 14:59:30 +0200

    add image extensions to file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
 	},
 	module: {
 		loaders: [{
-			test: /\.(eot|ttf|woff|woff2|svg)$/,
+			test: /\.(eot|ttf|woff|woff2|svg|png|jpg|gif])$/,
 			loader: 'file-loader'
 		},
 		{
